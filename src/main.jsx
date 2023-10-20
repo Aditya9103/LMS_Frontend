88830a1b936dd3c75c2dfc52fae7f9e5dc30a47f
Aditya9103 <adityakumarsingh84260@gmail.com>
2023-10-20 19:47:54 +0530

    Configured auto import sort

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
+//component import
+import './index.css';
+import App from './App.jsx'
+// css import 
+
+
+// library import
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    
   
 )
