acddb720e0094e4418422b652c885a65705dfb45
Aditya9103 <adityakumarsingh84260@gmail.com>
2023-10-31 22:42:08 +0530

    Added course detail component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 import AboutUs from './Pages/AboutUs';
+import Contact from './Pages/Contact';
+import CourseDescription from './Pages/Course/CourseDescription';
 import CourseList from './Pages/Course/CourseList';
+import Denied from './Pages/Denied';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/Login';
 import NotFound from './Pages/NotFound';
 import Signup from './Pages/Signup';
-import Contact from './Pages/Contact';
-import Denied from './Pages/Denied';
 function App() {
 
   return (
@@ -18,10 +19,14 @@ function App() {
         <Route path="/" element={<HomePage />} ></Route>
         <Route path="/about" element={<AboutUs />} ></Route>\
         <Route path="/courses" element={<CourseList />} />
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/denied" element={<Denied />} />
+
+        <Route path="/course/description" element={<CourseDescription />} />
+
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path='/denied' element={<Denied/>}/>
+
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
